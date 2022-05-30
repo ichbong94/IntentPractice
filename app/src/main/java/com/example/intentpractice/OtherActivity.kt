ee8c89efe5f6cd032fbc8b1cdd94aad2c4e98ce6
ichbong94 <ichbong94@gmail.com>
2022-05-30 23:41:08 +0900

    Intent 간 메시지 전달

--- app/src/main/java/com/example/intentpractice/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice/OtherActivity.kt
@@ -3,6 +3,8 @@ package com.example.intentpractice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
@@ -12,9 +14,11 @@ class OtherActivity : AppCompatActivity() {
 
         returnToMainBtn.setOnClickListener {
 
-//             실제로는 화면을 MainActivity를 더 만드르로 finish를 사용함            
+//             실제로는 화면을 MainActivity를 더 만드르로 finish를 사용함
 //            val myIntent = Intent(this, MainActivity::class.java)
 //            startActivity(myIntent)
+//            returnToMainBtn.text = R.layout.activity_main.moveToOtherBtn.text
+
             
             finish()
         }
