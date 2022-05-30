47e1b8dd8139054c6789478edf0565a140a32813
ichbong94 <ichbong94@gmail.com>
2022-05-30 22:36:14 +0900

    finish로 현재 종료

--- app/src/main/java/com/example/intentpractice/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice/OtherActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+//             실제로는 화면을 MainActivity를 더 만드르로 finish를 사용함            
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            
+            finish()
+        }
     }
 }
