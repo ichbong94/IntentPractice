317f31a4a0900b7cd57b0ed399f81ab4700a0c5d
ichbong94 <ichbong94@gmail.com>
2022-05-31 12:56:46 +0900

    전화 연결 예제

--- app/src/main/java/com/example/intentpractice/EditNicknameActivity.kt
+++ app/src/main/java/com/example/intentpractice/EditNicknameActivity.kt
@@ -1,11 +1,27 @@
 package com.example.intentpractice
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_editnickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editnickname)
+
+        okBtn.setOnClickListener {
+
+            val inputNewNickName = newNickNameEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNewNickName )
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.intentpractice
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_FOR_NICKNAME = 1005
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNicknameBtn.setOnClickListener {
+            val myIntent = Intent(this,EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
+
+
+
+        }
 
         sendMessageBtn.setOnClickListener {
 
@@ -29,6 +40,20 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, OtherActivity::class.java)
             startActivity(myIntent)
 
+           }
+
+      }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_FOR_NICKNAME){
+
+            if(resultCode == Activity.RESULT_OK){
+
+                val newNickname = data?.getStringExtra("nickname")
+                nickNameTxt.text = newNickname
+            }
         }
     }
 }
